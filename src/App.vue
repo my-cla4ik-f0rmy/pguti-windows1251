1d48694679e615c932afe6bf1225996fbc001d3e
my-cla4ik-f0rmy <retiree-frets-0r@icloud.com>
2024-02-01 03:12:28 +0400

    Revert "v2.0"
    This reverts commit 3d17a727416fe21f4aaf1f65f2ce2e45b5105fa3.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup>
-import app from './components/programm.vue'
-import Header from './components/header.vue'
+import HelloWorld from './components/programm.vue'
 import '@material/web/all'
 </script>
 
 <template>
-  <Header></Header>
-  <div class="programm">
-    <md-list>
-      <md-list-item>
-        <h1 style="overflow: visible;">Windows-1251 | 🌀ПГУТИ</h1>
-        <h4>Сайт для выполнения практических и лабораторных работ в ПГУТИ</h4>
-        Позволяет переводить:
-        <ul>
-          <li>Windows-1251 в текст</li>
-          <li>Текст в Windows-1251 и двоичный код*</li>
-          <li>Двоичный код* в текст и Windows-1251</li>
-        </ul>
-        <blockquote>*Двоичный код это перевод числа Windows-1251 из десятичной в двоичную систему счисления</blockquote>
-      </md-list-item>
-    </md-list>
-
-    <app></app>
-  </div>
+  <HelloWorld/>
 </template>
 
 
