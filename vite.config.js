bb2fe049a3d946d00fdd3f9036d91d4c7ca729c0
my-cla4ik-f0rmy <retiree-frets-0r@icloud.com>
2024-02-01 21:56:23 +0400

    upadate

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/testRepo/',
+  base: '/pguti-windows1251/',
   plugins: [vue({
     template: {
       compilerOptions: {
