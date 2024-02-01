a2a7e407c40af5e750e8d5115bddb56bd5c400c6
my-cla4ik-f0rmy <retiree-frets-0r@icloud.com>
2024-02-01 17:45:44 +0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/testRepo/',
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('md-')
+      }
+    }
+  })],
+})
